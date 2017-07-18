4f41239b169bc3b443543366e55866f71e76bba6
smlsunxie <smlsun@gmail.com>
2017-07-18 23:59:14 +0800

    優化 service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -7,7 +7,7 @@ use Exception;
 class UserService {
 
   public function findAll() {
-    return User::paginate(10);
+    return User::all();
   }
 
   public function findOne($id) {
@@ -15,26 +15,17 @@ class UserService {
   }
   
   public function update($user) {
-    if($user['name'] == "" || $user['email'] == ""){
-      throw new Exception("請輸入完整資料");
-    }
-    elseif(!preg_match("/.+@.+\.+.[a-zA-Z]{1,4}$/", $user["email"])){
-      throw new Exception("信箱格式錯誤");
-    }
-    else{
-      $mail = User::where('id','!=',$user['id'])->where('email',$user["email"])->get();
 
-      if(count($mail)==0){
-        $updateUser =  User::find($user["id"]);
-        $updateUser -> name = $user["name"];
-        $updateUser -> email = $user["email"];
-        $updateUser -> save();
+    try {
+      $this->verify($user);      
+      $updateUser =  User::find($user["id"]);
+      $updateUser -> name = $user["name"];
+      $updateUser -> email = $user["email"];
+      $updateUser -> save();
 
-        return $updateUser;
-      }
-      else{
-        throw new Exception("信箱已被註冊，無法使用");
-      }
+      return $updateUser;
+    } catch (Exception $e) {
+      throw $e;
     }
   }
   
@@ -43,30 +34,29 @@ class UserService {
   }
   
   public function save($user) {
-
-    if($user["email"] == "" || $user['name']=="") {
-      throw new Exception("請輸入完整資料");
-    }
-    elseif(!preg_match("/.+@.+\.+.[a-zA-Z]{1,4}$/", $user["email"])){
-      throw new Exception("信箱格式錯誤");
-    }
-    else{
-      $mail = User::where('email',$user["email"])->get();
-
-      if( count($mail)==0 ){
-        $createdUser = User::create([
-          'name' => $user["name"], 
-          'email' => $user["email"],
-          'password' => bcrypt('123456'),
-          'remember_token' => 1
-        ]);
-
-        return $createdUser;
-      }else{
-        throw new Exception("信箱已被註冊，無法使用");
-      }
+    try {
+      $this->verify($user);      
+      $createdUser = User::create([
+        'name' => $user["name"], 
+        'email' => $user["email"],
+        'password' => bcrypt('123456'),
+        'remember_token' => 1
+      ]);
+      return $createdUser;
+    } catch (Exception $e) {
+      throw $e;
     }
   }
 
-
+  public function verify($user) {
+    if($user["email"] == "" || $user['name']=="") {
+      throw new Exception("請輸入完整資料");
+    }
+    if(!preg_match("/.+@.+\.+.[a-zA-Z]{1,4}$/", $user["email"])){
+      throw new Exception("信箱格式錯誤");
+    }
+    $mail = User::where('id','!=',$user['id'])->where('email',$user["email"])->get();
+    if( count($mail) > 0 )
+      throw new Exception("信箱已被註冊，無法使用");
+  }
 }
